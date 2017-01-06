32868f69f3f26cb78845636f8083596ba230e393
sakura <sakurai@kazusa.or.jp>
2017-01-06 12:22:42 +0900

    最初のコミット

---
+++ Java.Sakurai.TestGit/src/test/TestMain.java
@@ -0,0 +1,15 @@
+package test;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        new TestMain();
+    }
+
+    public TestMain(){
+
+        System.out.println("This is test!");
+
+    }
+
+}
