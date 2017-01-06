1ece340cca6c52edcef24441b506fc7dc053ea4f
sakura <sakurai@kazusa.or.jp>
2017-01-06 13:08:04 +0900

    ignoreのテストで。

--- Java.Sakurai.TestGit/src/test/TestMain.java
+++ Java.Sakurai.TestGit/src/test/TestMain.java
@@ -1,5 +1,7 @@
 package test;
 
+import test.main.ServiceMain;
+
 public class TestMain {
 
     public static void main(String[] args) {
@@ -8,7 +10,7 @@ public class TestMain {
 
     public TestMain(){
 
-        System.out.println("This is a test!");
+        new ServiceMain();
 
     }
 
---
+++ Java.Sakurai.TestGit/src/test/main/ServiceMain.java
@@ -0,0 +1,15 @@
+package test.main;
+
+public class ServiceMain {
+
+    public ServiceMain(){
+
+        System.out.println("This is test3!!");
+
+    }
+
+    public static void showMessage(){
+
+        System.out.println("This is test4!!");
+    }
+}
